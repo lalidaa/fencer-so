f400bdafde7e25cb83ff5c68abc8364b90d4988f
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-04-09 17:22:53 -0600

    Modifications as per assigned task
    CommBank.csproj changes:
    [*] updated MongoDB driver to 2.24.0 version due to security issues
    Goal.cs changes:
    [*] added icon field to the code as per requirements

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -24,7 +24,9 @@ public class Goal
 
     [BsonRepresentation(BsonType.ObjectId)]
     public List<string>? TagIds { get; set; }
-
+    
+    public string? Icon { get; set; }
+    
     [BsonRepresentation(BsonType.ObjectId)]
     public string? UserId { get; set; }
 }
